c132a6a03727cb1a00208e92e420a070b9afbe33
Travis <travis@android-8efa3f256-boca70a.lan>
2023-09-15 22:08:12 -0700

    use clang-format on c files

--- src/case.h
+++ src/case.h
@@ -1,76 +1,50 @@
 #include <stdbool.h>
 
 typedef enum {
-    CASE_CAMEL,
-    CASE_PASCAL,
-    CASE_SNAKE,
-    CASE_CONSTANT,
-    CASE_KEBAB,
-    CASE_HEADER,
-    CASE_LOWER,
-    CASE_UPPER,
-    CASE_CAPITAL,
-    CASE_UNKNOWN, 
+  CASE_CAMEL,
+  CASE_PASCAL,
+  CASE_SNAKE,
+  CASE_CONSTANT,
+  CASE_KEBAB,
+  CASE_HEADER,
+  CASE_LOWER,
+  CASE_UPPER,
+  CASE_CAPITAL,
+  CASE_UNKNOWN,
 } Case;
 
-char* case_upper(
-  const char* text, 
-  unsigned long text_len, 
-  char* fill_text, 
-  unsigned long fill_len, 
-  char keep_apostrophes
-);
+char *case_upper(const char *text, unsigned long text_len, char *fill_text,
+                 unsigned long fill_len, char keep_apostrophes);
 
-char* case_lower(
-  const char* text, 
-  unsigned long text_len, 
-  char* fill_text, 
-  unsigned long fill_len, 
-  char keep_apostrophes
-);
+char *case_lower(const char *text, unsigned long text_len, char *fill_text,
+                 unsigned long fill_len, char keep_apostrophes);
 
-char* case_capital(
-  const char* text, 
-  unsigned long text_len, 
-  char* fill_text, 
-  unsigned long fill_len, 
-  char keep_apostrophes
-);
+char *case_capital(const char *text, unsigned long text_len, char *fill_text,
+                   unsigned long fill_len, char keep_apostrophes);
 
-char* case_header(const char* text, unsigned long text_len);
-char* case_constant(const char* text, unsigned long text_len);
-char* case_snake(const char* text, unsigned long text_len);
-char* case_kebab(const char* text, unsigned long text_len);
-char* case_camel(const char* text, unsigned long text_len);
-char* case_pascal(const char* text, unsigned long text_len);
+char *case_header(const char *text, unsigned long text_len);
+char *case_constant(const char *text, unsigned long text_len);
+char *case_snake(const char *text, unsigned long text_len);
+char *case_kebab(const char *text, unsigned long text_len);
+char *case_camel(const char *text, unsigned long text_len);
+char *case_pascal(const char *text, unsigned long text_len);
 
-char* case_to_ext(
-  Case case_id, 
-  const char* text, 
-  unsigned long text_len,
-  char* fill_text, 
-  unsigned long fill_len, 
-  char keep_apostrophes
-);
+char *case_to_ext(Case case_id, const char *text, unsigned long text_len,
+                  char *fill_text, unsigned long fill_len,
+                  char keep_apostrophes);
 
+bool case_is_lower(const char *text, unsigned long text_len);
+bool case_is_upper(const char *text, unsigned long text_len);
+bool case_is_capital(const char *text, unsigned long text_len);
+bool case_is_camel(const char *text, unsigned long text_len);
+bool case_is_pascal(const char *text, unsigned long text_len);
+bool case_is_snake(const char *text, unsigned long text_len);
+bool case_is_kebab(const char *text, unsigned long text_len);
+bool case_is_header(const char *text, unsigned long text_len);
+bool case_is_constant(const char *text, unsigned long text_len);
 
-bool case_is_lower(const char* text, unsigned long text_len);
-bool case_is_upper(const char* text, unsigned long text_len);
-bool case_is_capital(const char* text, unsigned long text_len);
-bool case_is_camel(const char* text, unsigned long text_len);
-bool case_is_pascal(const char* text, unsigned long text_len);
-bool case_is_snake(const char* text, unsigned long text_len);
-bool case_is_kebab(const char* text, unsigned long text_len);
-bool case_is_header(const char* text, unsigned long text_len);
-bool case_is_constant(const char* text, unsigned long text_len);
-
-char* case_to(Case case_id, const char* text, unsigned long text_len);
-char* case_to_buf(
-  Case case_id,
-  const char* text,
-  unsigned long text_len,
-  char* buf,
-  unsigned long buf_len
-);
-char  case_of(const char* text, unsigned long text_len);
-char* case_id_to_string(Case case_id);
+char *case_to(Case case_id, const char *text, unsigned long text_len);
+char *case_to_buf(Case case_id, const char *text, unsigned long text_len,
+                  char *buf, unsigned long buf_len);
+char case_of(const char *text, unsigned long text_len);
+char *case_id_to_string(Case case_id);
--- src/test.c
+++ src/test.c
@@ -1,30 +1,30 @@
 // run this file
-// $ zig build && zig run src/test.c -lc zig-out/lib/libcase.a 
+// $ zig build && zig run src/test.c -lc zig-out/lib/libcase.a
 // valgrind leak check
-// $ zig build && zig build-exe src/test.c -lc zig-out/lib/libcase.a && valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes test
+// $ zig build && zig build-exe src/test.c -lc zig-out/lib/libcase.a && valgrind
+// --leak-check=full --show-leak-kinds=all --track-origins=yes test
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 #include "case.h";
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main(void)
-{
-  char* s = "foo_bar";
+int main(void) {
+  char *s = "foo_bar";
   int slen = strlen(s);
   {
-    char* actual =  case_upper(s, slen, NULL, 0, 1);
-    char* expected = "FOO BAR";
-    if(memcmp(expected, actual, slen) != 0) {
+    char *actual = case_upper(s, slen, NULL, 0, 1);
+    char *expected = "FOO BAR";
+    if (memcmp(expected, actual, slen) != 0) {
       printf("ERROR: expected '%s' got '%s'\n", expected, actual);
       exit(1);
     }
     free(actual);
   }
   {
-    char* actual =  case_to(CASE_HEADER, s, slen);
-    char* expected = "Foo-Bar";
-    if(memcmp(expected, actual, slen) != 0) {
+    char *actual = case_to(CASE_HEADER, s, slen);
+    char *expected = "Foo-Bar";
+    if (memcmp(expected, actual, slen) != 0) {
       printf("ERROR: expected '%s' got '%s'\n", expected, actual);
       free(actual);
       exit(1);
@@ -34,15 +34,15 @@ int main(void)
   char buf[20];
   int buflen = sizeof(buf);
   {
-    char* actual =  case_to_buf(CASE_CONSTANT, s, slen, buf, buflen);
-    char* expected = "FOO_BAR";
-    if(memcmp(expected, actual, slen) != 0) {
+    char *actual = case_to_buf(CASE_CONSTANT, s, slen, buf, buflen);
+    char *expected = "FOO_BAR";
+    if (memcmp(expected, actual, slen) != 0) {
       printf("ERROR: expected '%s' got '%s'\n", expected, actual);
       exit(1);
     }
   }
 
-  const char* expecteds[CASE_UNKNOWN];
+  const char *expecteds[CASE_UNKNOWN];
   expecteds[CASE_CAMEL] = "fooBar";
   expecteds[CASE_PASCAL] = "FooBar";
   expecteds[CASE_SNAKE] = "foo_bar";
@@ -52,38 +52,38 @@ int main(void)
   expecteds[CASE_LOWER] = "foo bar";
   expecteds[CASE_UPPER] = "FOO BAR";
   expecteds[CASE_CAPITAL] = "Foo Bar";
-  
+
   // test all cases
   for (char i = 0; i < CASE_UNKNOWN; ++i) {
-    const char* expected = expecteds[i];
+    const char *expected = expecteds[i];
     int len = strlen(expected);
-    char* case_str = case_id_to_string(i);
+    char *case_str = case_id_to_string(i);
     { // case_to
-      char* actual = case_to(i, s, slen);
+      char *actual = case_to(i, s, slen);
       // printf("slen=%d expected=%s actual=%s\n", slen, expected, actual);
-      if(memcmp(expected, actual, len) != 0) {
-        printf("ERROR: expected to-%s '%s':%u got '%s':%d\n", 
-          case_str, expected, len, actual, strlen(actual));
+      if (memcmp(expected, actual, len) != 0) {
+        printf("ERROR: expected to-%s '%s':%u got '%s':%d\n", case_str,
+               expected, len, actual, strlen(actual));
         free(actual);
         exit(1);
       }
       free(actual);
     }
     { // case_to_buf
-      char* actual = case_to_buf(i, s, slen, buf, buflen);
+      char *actual = case_to_buf(i, s, slen, buf, buflen);
       // printf("slen=%d expected=%s actual=%s\n", slen, expected, actual);
-      if(memcmp(expected, actual, len) != 0) {
-        printf("ERROR: expected to-%s '%s':%u got '%s':%d\n", 
-          case_str, expected, len, actual, strlen(actual));
+      if (memcmp(expected, actual, len) != 0) {
+        printf("ERROR: expected to-%s '%s':%u got '%s':%d\n", case_str,
+               expected, len, actual, strlen(actual));
         exit(1);
       }
     }
     { // case_of
       char case_id = case_of(expected, len);
       // printf("case_id=%d str=%s\n", case_id, case_str);
-      if(case_id != i) {
-        printf("ERROR: expected case_of('%s')='%s'. got %u='%s'\n", 
-          expected, case_str, case_id, case_id_to_string(case_id));
+      if (case_id != i) {
+        printf("ERROR: expected case_of('%s')='%s'. got %u='%s'\n", expected,
+               case_str, case_id, case_id_to_string(case_id));
         exit(1);
       }
     }
