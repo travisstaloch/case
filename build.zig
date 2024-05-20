698a01886de891c03434f34e6bca95546026eb0f
Travis Staloch <1562827+travisstaloch@users.noreply.github.com>
2024-05-19 19:21:56 -0700

    chore: update to zig version 0.13.0-dev.211+6a65561e3

--- build.zig
+++ build.zig
@@ -5,12 +5,12 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
     const mod = b.addModule(
         "case",
-        .{ .root_source_file = .{ .path = "src/lib.zig" } },
+        .{ .root_source_file = b.path("src/lib.zig") },
     );
 
     const lib = b.addStaticLibrary(.{
         .name = "case",
-        .root_source_file = .{ .path = "src/c_api.zig" },
+        .root_source_file = b.path("src/c_api.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -18,18 +18,18 @@ pub fn build(b: *std.Build) void {
     lib.linkLibC();
     b.installArtifact(lib);
     b.getInstallStep().dependOn(
-        &b.addInstallHeaderFile(.{ .path = "src/case.h" }, "case.h").step,
+        &b.addInstallHeaderFile(b.path("src/case.h"), "case.h").step,
     );
 
     const tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/tests.zig" },
+        .root_source_file = b.path("src/tests.zig"),
         .target = target,
         .optimize = optimize,
     });
     tests.linkLibC();
     tests.root_module.addImport("case", mod);
     // needed to keep Case enums in sync between src/lib.zig and src/case.h
-    tests.addIncludePath(.{ .path = "src" });
+    tests.addIncludePath(b.path("src"));
     const run_tests = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_tests.step);
@@ -41,7 +41,7 @@ pub fn build(b: *std.Build) void {
         .target = target,
         .optimize = optimize,
     });
-    c_api_test_exe.addCSourceFile(.{ .file = .{ .path = "src/test.c" } });
+    c_api_test_exe.addCSourceFile(.{ .file = b.path("src/test.c") });
     c_api_test_exe.linkLibC();
     c_api_test_exe.linkLibrary(lib);
     const run_c_api_test = b.addRunArtifact(c_api_test_exe);
